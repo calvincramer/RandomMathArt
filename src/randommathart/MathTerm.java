1ca4ae3bb4d548fdbcf77b37d699fc1541be9582
Calvin Cramer <cjcramer@ucdavis.edu>
2018-12-19 23:51:17 -0800

    Small outline

--- src/randommathart/MathTerm.java
+++ src/randommathart/MathTerm.java
@@ -9,4 +9,26 @@ public class MathTerm {
     //pi / integer
     //sin(term), cos(term), tan(term)
     //pow(term, num)
+    
+    public static final int RANDOM = 0;
+    public static final int PI_TERM = 1;
+    public static final int SIN = 2;
+    public static final int COS = 3;
+    public static final int TAN = 4;
+    public static final int POW = 5;
+    public static final int X   = 6;
+    public static final int Y   = 7;
+    
+    private int state = -1;
+    
+    public MathTerm(int mathTerm) {
+        if (mathTerm < 0 || mathTerm > 7) {
+            System.out.println("BAD MATH TERM");
+        }
+        this.state = mathTerm;
+    }
+    
+    public double evaluate(double x, double y) {
+        
+    }
 }
